104337c3f89dfa63c6524dcf088e558d2e6136f0
remiv1 <remiv1@gmail.com>
2026-02-13 14:24:19 +0100

    feat: Nettoyage des fichiers de données de test et mise à jour des tests unitaires pour la classe Customers et Users

--- tests/db_objects/test_customers.py
+++ tests/db_objects/test_customers.py
@@ -1,23 +1,17 @@
 """Test unitaire pour la classe Customers dans db_models.objects.customers."""
 
+from sqlalchemy.orm import Session
 from db_models.objects.customers import Customers
-from db_models.objects.orders import Order, OrderLine
-from db_models.objects.objects import GeneralObjects, Books, OtherObjects, MediaFiles, ObjMetadatas, Tags, ObjectTags
-from db_models.objects.shipments import Shipment
-from db_models.objects.inventory import InventoryMovements
-from db_models.objects.invoices import Invoice
-from db_models.objects.suppliers import Suppliers
-from db_models.objects.users import Users
-from tests.fake_datas.sqlite_fixture import db_session, engine
+from tests.fake_datas.sqlite_fixture import db_session, engine  # pylint: disable=unused-import # type: ignore
 
-def test_customer_creation(db_session) -> None:
+def test_customer_creation(db_session: Session) -> None:    # pylint: disable=redefined-outer-name
     """Test de la méthode to_dict de la classe Customers avec des données issues de la fixture."""
     customer = Customers(wpwc_id="1", henrri_id="2", customer_type="pro", is_active=True)
     db_session.add(customer)
     db_session.commit()
     assert db_session.query(Customers).count() == 1
 
-def test_customer_read(db_session) -> None:
+def test_customer_read(db_session: Session) -> None:    # pylint: disable=redefined-outer-name
     """Test pour vérifier que le client créé est bien retrouvé"""
     customer = Customers(wpwc_id="1", henrri_id="2", customer_type="pro", is_active=True)
     db_session.add(customer)
@@ -26,24 +20,23 @@ def test_customer_read(db_session) -> None:
     new_customer = Customers(wpwc_id="ojg54561", henrri_id="oe65v06b5g106e", customer_type="part")
     db_session.add(new_customer)
     db_session.commit()
+    assert customer.id == 1 if customer else None
+    assert customer.customer_type == 'pro' if customer else None
+    assert customer.is_active is True if customer else None
 
-    assert customer.id == 1
-    assert customer.customer_type == 'pro'
-    assert customer.is_active == True
-
-def test_new_customer_write(db_session) -> None:
+def test_new_customer_write(db_session: Session) -> None:    # pylint: disable=redefined-outer-name
     """test de lecture du client rentré précédemment et de modification"""
     customer = Customers(wpwc_id="1", henrri_id="2", customer_type="pro", is_active=True)
     db_session.add(customer)
     db_session.commit()
-    customer = db_session.query(Customers).where(Customers.id==1).first()
     new_customer = Customers(wpwc_id="ojg54561", henrri_id="oe65v06b5g106e", customer_type="part")
     db_session.add(new_customer)
     db_session.commit()
     customer = db_session.query(Customers).where(Customers.id==2).first()
-    customer.is_active = False
+    if customer:
+        customer.is_active = False
     db_session.add(customer)
     db_session.commit()
     customer = db_session.query(Customers).where(Customers.id==2).first()
-    assert customer.is_active == False
-    assert customer.customer_type == 'part'
+    assert customer.is_active is False if customer else None
+    assert customer.customer_type == 'part' if customer else None
--- tests/fake_datas/sqlite_fixture.py
+++ tests/fake_datas/sqlite_fixture.py
@@ -5,6 +5,16 @@ import pytest
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
 from db_models import WorkingBase, SecureBase
+from db_models.objects.orders import Order, OrderLine   # type: ignore # pylint: disable=unused-import
+from db_models.objects.objects import (     # type: ignore # pylint: disable=unused-import
+    GeneralObjects, Books, OtherObjects, MediaFiles, ObjMetadatas, Tags, ObjectTags     # pylint: disable=unused-import    # type: ignore 
+)   # type: ignore # pylint: disable=unused-import
+from db_models.objects.shipments import Shipment    # pylint: disable=unused-import    # type: ignore
+from db_models.objects.inventory import InventoryMovements  # type: ignore # pylint: disable=unused-import
+from db_models.objects.invoices import Invoice  # type: ignore # pylint: disable=unused-import
+from db_models.objects.suppliers import Suppliers   # type: ignore # pylint: disable=unused-import
+from db_models.objects.users import Users   # type: ignore # pylint: disable=unused-import
+
 
 @pytest.fixture(scope="session")
 def engine() -> Engine:
@@ -12,17 +22,17 @@ def engine() -> Engine:
     return create_engine("sqlite:///tests/fake_datas/test_db.sqlite")
 
 @pytest.fixture(scope="function")
-def db_session(engine: Engine) -> Session:
+def db_session(engine: Engine) -> Session:  # pylint: disable=redefined-outer-name # type: ignore
     """Crée toutes les tables dans la base de test"""
     WorkingBase.metadata.create_all(engine)
     SecureBase.metadata.create_all(engine)
     connection = engine.connect()
     transaction = connection.begin()
     session_factory = sessionmaker(bind=connection)
-    Session = scoped_session(session_factory)
-    session = Session()
+    local_session = scoped_session(session_factory)  # pylint: disable=redefined-outer-name, invalid-name # type: ignore
+    session = local_session()
 
-    yield session  # Fournit la session au test
+    yield session  # Fournit la session au test # type: ignore
 
     session.close()
     transaction.rollback()
