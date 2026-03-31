82aa2a7d18c3e2c62f0986e2072628f2393e3f3f
remiv1 <remiv1@gmail.com>
2026-03-31 20:33:27 +0200

    fix: refactor la création des sessions SQLAlchemy pour simplifier le code et éviter les pools trop importants

--- app_front/config/db_conf.py
+++ app_front/config/db_conf.py
@@ -13,20 +13,21 @@ SECURE_DATABASE_URL = getenv(
 )
 MONGODB_URL = getenv("MONGODB_URL", "mongodb://app:pwd@db-logs:27017/sauvetage_logs")
 
+_engine_main = create_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
+_SessionMain = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=_engine_main))
+
 
 def get_main_session():
-    """Crée une session SQLAlchemy pour la base de données principale."""
+    """Retourne une session SQLAlchemy pour la base de données principale."""
 
     # Gestion des sessions pour les tests.
     if hasattr(current_app, "db_session_main"):
         return current_app.db_session_main  # pylint: disable=no-member # type: ignore
-    engine = create_engine(
-        DATABASE_URL,
-        echo=False,
-        pool_size=10,
-        max_overflow=20,
-        pool_pre_ping=True,
-        pool_recycle=3600,
-    )
-    _session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-    return _session()
+    return _SessionMain()
